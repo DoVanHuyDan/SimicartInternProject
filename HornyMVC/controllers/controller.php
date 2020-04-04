e1d43691d7dd9a022558d8f4442ca4af0fbfd492
Dan <huy.dv1999@gmail.com>
2020-04-04 17:53:44 +0700

    Done_  upload img added

--- HornyMVC/controllers/controller.php
+++ HornyMVC/controllers/controller.php
@@ -15,33 +15,66 @@ class Controller extends DbInteractions
         include_once 'views/detail.php';
     }
 
-    public function showUpdateForm($id)
+    public function showUpdateForm($id = null)
     {
+
         include_once 'views/update.php';
     }
 
-    public function update($id, $name, $price, $image)
+    public function deleteImage($path)
     {
-        $this->updateOneRecord($id, $image, $name, $price);
-        echo '
-            <div class="alert alert-primary" role="alert">
-            Updated!
-            </div>
-            
-            ';
+        if (unlink($path)) {
+            // delete successfully
+        } else {
+            echo "you have an error deleting image of your product";
+        }
+    }
+
+    public function update($id, $name, $price, $oldImage)
+    {
+
+        $newImage = $this->uploadFile();
+        if ( !empty($name) )  {
+            if (empty($newImage)) // if user did upload any new image when updating 
+            {
+                $this->updateOneRecord($id, $oldImage, $name, $price);
+            } else {
+                // delete old oldImage if user uploaded new image
+                $this->deleteImage($oldImage);
+                $this->updateOneRecord($id, $newImage, $name, $price);
+            }
+            echo '
+                <div class="alert alert-primary" role="alert">
+                Updated!
+                </div>           
+                ';
+        } else {
+            echo '
+                
+                <div class="alert alert-danger" role="alert">
+                 you need to fill up all fields!
+                </div>
+                
+                ';
+        }
+
         // after update, show all products 
         include_once 'views/viewall.php';
     }
 
     public function deleteOneRecord($id)
     {
+        
+        $this->deleteImage($this->getOneRecord($id)['image']);
         $this->deleteRecord($id);
         // after delete, show all products 
-        include_once 'views/viewall.php';
+        header("location: index.php");
+       // include_once 'views/viewall.php';
     }
 
-    public function createNew($name, $price, $image)
-    {
+    public function createNew($name, $price)
+    {    // assign $image  = path to the uploaed image
+        $image = $this->uploadFile();
 
         if (empty($name) || empty($price) || empty($image)) {
             echo '
@@ -51,11 +84,14 @@ class Controller extends DbInteractions
                 </div>
                 
                 ';
-            include_once 'views/createnew.php';
-        }
-        else{
+            include_once 'views/update.php';
+        } else {
+
+
             $this->createNewRecord($image, $name, $price);
+
             echo '
+
                 <div class="alert alert-primary" role="alert">
                 Created and saved!
                 </div>
@@ -64,11 +100,56 @@ class Controller extends DbInteractions
             // after created , show all
             include_once 'views/viewall.php';
         }
-       
     }
+
+
+    public function uploadFile()
+    {
+        // if users do not upload any file -> return null
+        if (empty($_FILES['image']['size'])) {
+            return null;
+        } else {
+
+            $fileName = $_FILES['image']['name'];
+            $fileTmpName = $_FILES['image']['tmp_name'];
+            $fileSize = $_FILES['image']['size'];
+            $fileError = $_FILES['image']['error'];
+
+
+
+            $fileEXT = explode('.', $fileName);
+            $actuallfileEXT = strtolower(end($fileEXT));
+
+            $allowed = array('png', 'jpg', 'jpeg', 'pdf');
+            if (in_array($actuallfileEXT, $allowed)) {
+                if ($fileError === 0) {
+
+                    if ($fileSize < 10000000) {
+                        // uniqid method to creat unique number , then we will never have same file names
+                        $fileNameNew = uniqid('', true) . '.' . $actuallfileEXT;
+                        $destinationFolder = 'upload/' . $fileNameNew;
+
+                        // uploadfile 
+                        move_uploaded_file($fileTmpName, $destinationFolder);
+                        ///     header("location : php.php");
+                        return $destinationFolder;
+                    } else {
+                        echo 'your file is too big';
+                    }
+                } else {
+                    echo 'there is an error uploading your file!';
+                }
+            } else {
+                echo 'your file is not allowed to upload!';
+            }
+        }
+    }
+
     // method to handle requests sent by GET
     public function handleRequests()
     {
+
+        // if user click on any links / button on viewall page , means that param has been sent here by GET
         if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
             $op = isset($_GET['op']) ? $_GET['op'] : null;
@@ -85,19 +166,22 @@ class Controller extends DbInteractions
                     $this->deleteOneRecord($_GET['id']);
                 } elseif ($op == "showUpdateForm") {
                     $this->showUpdateForm($_GET['id']);
+                } elseif ($op == "createnew") {
+                    $this->showUpdateForm();
                 }
             } catch (Exception $e) {
                 echo $e->getMessage();
             }
-        } else {
+        } elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-            // op just will be sent by POST when user updates data for a product
+            // op just will be sent by POST when user updates data for a product / create  new one
             $op = isset($_GET['op']) ? $_GET['op'] : null;
 
             if ($op == "update") {
-                $this->update($_GET['id'], $_POST['name'], $_POST['price'], $_POST['image']);
+
+                $this->update($_GET['id'], $_POST['name'], $_POST['price'], $_POST['oldImage']);
             } elseif ($op == "createnew") {
-                $this->createNew($_POST['name'], $_POST['price'], $_POST['image']);
+                $this->createNew($_POST['name'], $_POST['price']);
             }
         }
     }
--- HornyMVC/views/detail.php
+++ HornyMVC/views/detail.php
@@ -12,7 +12,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="css/css.css">
+    <link rel="stylesheet" href="../css/css.css">
 </head>
 
 <body>
@@ -33,11 +33,11 @@
     <div class="container">
 
         <div class="page-header">
-            <h1>Read Product</h1>
+            <h1> Product Details</h1>
         </div>
 
 
-        <table class='table table-hover table-responsive table-bordered'>
+        <table class='table table-hover table-responsive '>
             <tr>
                 <th>Name</th>                  
                 <td><?php echo htmlspecialchars($record['name'], ENT_QUOTES);  ?></td>
@@ -48,7 +48,7 @@
             </tr>
             <tr>
                 <th>Image</th>
-                <td><img src="<?php echo $record['image'] ?>" alt="image for this product"></td>
+                <td><img src="<?php echo $record['image']; ?>" alt="image for this product"></td>
             </tr>
             <tr>
                 <td></td>
